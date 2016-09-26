422b606f7ca7ed507a04330efda5f3e6f4cb1499
Thomas Arrow <thomasarrow@gmail.com>
2016-09-26 14:39:57 +0100

    handle endpoint that isn't main api

--- index.js
+++ index.js
@@ -10,8 +10,12 @@ var endpointTestUrl = "https://sandbox.zenodo.org/api"
 //we should then check these are correct
 //and then we should push the information to zenodo
 
-function Zenodo() {
 
+//This should take: username, api-key, optionally the endpoint
+function Zenodo(user, key, endpoint) {
+  this.user = user
+  this.key = key
+  this.endpoint = endpoint || 'https://zenodo.org/api/'
 }
 
 module.exports=Zenodo
--- test/client.js
+++ test/client.js
@@ -3,7 +3,17 @@ var Zenodo = require("../index.js")
 
 describe ("API object", function () {
   it("returns an object on new keyword", function () {
-    var zenodo = new Zenodo('test')
+    var zenodo = new Zenodo()
     expect(zenodo).to.exist
+  })
+  it("correctly handles parameters pass in the constructor", function () {
+    var zenodo = new Zenodo('testuser', 'TESTAPIKEY')
+    expect(zenodo.user).to.equal('testuser')
+    expect(zenodo.key).to.equal('TESTAPIKEY')
+    expect(zenodo.endpoint).to.equal('https://zenodo.org/api/')
+  })
+  it("correctly handles passing the sandbox endoint", function () {
+    var zenodo = new Zenodo('testuser', 'TESTAPIKEY', 'https://sandbox.zenodo.org/api/')
+
   })
 })
