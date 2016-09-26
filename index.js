0d6dff1fc77554e9101a8992bdd2aec7e28943d4
Thomas Arrow <tom@contentmine.org>
2016-09-26 10:52:23 +0100

    initial index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+var endpointUrl = "https://zenodo.org/api"
+var endpointTestUrl = "https://sandbox.zenodo.org/api"
+
+//We should make a a new Upload object
+
+//We should then add things like metadata
+//The names of files
+//The username and api key
+
+//we should then check these are correct
+//and then we should push the information to zenodo
+ 
+
+exports['default']=zen-upload
